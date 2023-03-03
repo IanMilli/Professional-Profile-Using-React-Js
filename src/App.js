56dffe371ee3f6fbc624056359fa195eca4ee78e
Ian M <ianmillichamp.2@gmail.com>
2023-03-03 23:00:29 +0000

    added code to home.js to see if can get text to appear - success

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/Home'";
-import About from "./pages/About'";
-import Contacts from "./pages/Contacts'";
-import Projects from "./pages/Projects'";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+import Projects from "./pages/Projects";
+
+function Navbar () {return (
+  <nav>
+    <Link to ="/">Home</Link>
+    <Link to ="/">About</Link>
+    <Link to ="/">Projects</Link>
+   
+  </nav>
+)}
+
+
 
 function App() {
   return (
@@ -13,7 +24,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
-        <Route path="/contacts" element={<Contacts />} />
+       
       </Routes>
     </Router>
   );
