5470d0faf4c52813bd1254aa1e1e4a73da08848f
Ian M <ianmillichamp.2@gmail.com>
2023-03-03 22:49:57 +0000

    created links to navigate between pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 
-import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home'";
+import About from "./pages/About'";
+import Contacts from "./pages/Contacts'";
+import Projects from "./pages/Projects'";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contacts" element={<Contacts />} />
+      </Routes>
+    </Router>
   );
 }
 
