30c7fc0e0500e21810562ad442653a5eb5e327f9
Ian M <ianmillichamp.2@gmail.com>
2023-03-05 21:28:14 +0000

    added links to navbar and styled as required. not quite completed unhappy about how the dropdown titles change as per reduction to mobile size

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,25 +6,43 @@ import '../components/navbar.css';
 
 function NavBar() {
   return (
-    <Navbar  expand="lg" className='navigation'>
+    <Navbar expand="lg" className='navigation'>
+      <img src='../images/logoNav.png' href="#home"id="logoNav"></img>
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
+            </Nav>
+            <Nav >
+            <Nav.Link href="#home"className="home" ><h2>Home</h2></Nav.Link>
+            <Nav.Link href="#about"className="about"><h2>About</h2></Nav.Link>
+            <NavDropdown title="Projects" id="projects-nav-dropdown">
+              <NavDropdown.Item href="#professional">
+                Professional Projects
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
+              <NavDropdown.Item href="#Personal Development">Personal Development Projects</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#Voluntary">
+                Voluntary Work for Non Profits
               </NavDropdown.Item>
             </NavDropdown>
+            <NavDropdown title="Lets Connect!" id="connect-nav-dropdown">
+              <NavDropdown.Item href="tel:+4407498174271">
+                Phone
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="mailto:ianmillichamp.2@gmail.com">Email</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="https://www.linkedin.com/in/ian-millichamp/">
+                LinkedIn
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="https://github.com/IanMilli">
+                GitHub
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#resume"className="resume"><h2>Resume</h2></Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
