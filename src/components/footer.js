fe84bdae705ab697b5f31855d48fe61f9db885f4
Ian M <ianmillichamp.2@gmail.com>
2023-03-06 12:17:15 +0000

    resolved bug in js file for footer, added code to aid mobile responsiveness

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,7 +7,7 @@ import '../components/footer.css';
 function Footer() {
     return (
         <Navbar expand="lg" className='footer'>
-            <img src='../images/logoFoot.png' href="#home"id="logoNav" alt='logo Img'></img>
+            <img src='../images/logoFoot.png' href="#home"id="logoFoot" alt='logo Img'></img>
             <Container>
                 <Nav>
 
@@ -22,11 +22,11 @@ function Footer() {
                         
                     </a>
                     </div>
-                <Nav.Link className="text-info " href="tel:+4407498174271" id="phone"><h2><i className="fa fa-phone"></i></h2></Nav.Link>
-                <Nav.Link className="text-info " href="mailto:ianmillichamp.2@gmail.com"id="email"><h2><i className="fa fa-envelope"></i></h2></Nav.Link>
-                <Nav.Link className="text-info " href="https://www.linkedin.com/in/ian-millichamp/"id="linkedin">
-                    <h2> <i className="fa fa-linkedin"></i></h2></Nav.Link>
-                <Nav.Link className="text-info " href="https://github.com/IanMilli"id="github"><h2> <i className="fa fa-github"></i></h2></Nav.Link>
+                <Nav.Link className="text-info logo1" href="tel:+4407498174271" id="phone"><h2><i className="fa fa-phone"></i></h2></Nav.Link>
+                <Nav.Link className="text-info logo1" href="mailto:ianmillichamp.2@gmail.com"id="email"><h2><i className="fa fa-envelope"></i></h2></Nav.Link>
+                <Nav.Link className="text-info logo1" href="https://www.linkedin.com/in/ian-millichamp/"id="linkedin">
+                    <h2> <i className="fa fa-linkedin "></i></h2></Nav.Link>
+                <Nav.Link className="text-info logo1" href="https://github.com/IanMilli"id="github"><h2> <i className="fa fa-github"></i></h2></Nav.Link>
             </Nav>
         </Container>
         </Navbar >
