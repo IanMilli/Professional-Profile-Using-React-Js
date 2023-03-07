ca7ffaa15774ac18ed73378bdc9ca3ddc94b26c4
Ian M <ianmillichamp.2@gmail.com>
2023-03-07 19:18:51 +0000

    added routing information for multiple pages to finish basic routing structure of the site

--- src/App.js
+++ src/App.js
@@ -6,6 +6,17 @@ import Projects from "./pages/Projects";
 import Personal from "./pages/Personal";
 import Voluntary from "./pages/Voluntary";
 import Professional from "./pages/Professional";
+import Contact from "./pages/Contact";
+
+import BbCentre from "./pages/BbCentre.js";
+import IngrediantFy from "./pages/IngrediantFy";
+import Password from "./pages/Password";
+import Planner from "./pages/Planner";
+import Project2 from "./pages/Project2";
+import ReadMe from "./pages/ReadMe";
+import Revision from "./pages/Revision";
+import Weather from "./pages/Weather";
+
 import NavBar from './components/navbar.js';
 import Footer from './components/footer.js';
 
@@ -22,6 +33,16 @@ function App() {
         <Route path="/Personal" element={<Personal />} />
         <Route path="/Voluntary" element={<Voluntary />} />
         <Route path="/Professional" element={<Professional />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/BbCentre" element={<BbCentre />} />
+        <Route path="/IngrediantFys" element={<IngrediantFy />} />
+        <Route path="/Password" element={< Password />} />
+        <Route path="/Planner" element={<Planner />} />
+        <Route path="/Project2" element={<Project2 />} />
+        <Route path="/ReadMe" element={<ReadMe />} />
+        <Route path="/Revision" element={<Revision />} />
+        <Route path="/Weather" element={<Weather />} />
+
       </Routes>
       <Footer/>
     </Router>
