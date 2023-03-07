b508986fc9d957b1447e78dea474c5b71f9b5929
Ian M <ianmillichamp.2@gmail.com>
2023-03-07 13:22:15 +0000

    used bootstrap grid to arrange sections

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,29 @@
+import Container from 'react-bootstrap/Container';
 import React from "react";
 import Button from 'react-bootstrap/Button';
+import '../pages/Home.css';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 function Home() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui
-        mauris, ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus
-        porta. Nam quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam
-        semper imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed
-        rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
-      <Button variant="primary">Primary</Button>{' '}
-    </div>
+    <Container className="body">
+     <Row>
+        <Col> <div className="Profile-pic">
+    <img src='../images/profilepic.jpg'alt="profilepic" id="profilepic"></img>
+  </div></Col>
+        <Col> <div className="Text-about">
+    Welcome, To Learn More about Me, Ian Millichamp , a Front End Web Developer, Chef and Hospitality Manager Please Click the following link:
+  </div></Col>
+      </Row>
+      <Row>
+        <Col><div className="quote">
+    something something something, sean havens
+  </div></Col>
+      </Row>
+</Container>
+
   );
 }
 
