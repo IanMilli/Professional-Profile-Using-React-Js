50f9981e7e1b4528aacf95905ff09f4bd4893df7
Ian M <ianmillichamp.2@gmail.com>
2023-03-07 15:26:10 +0000

    set up about page

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import Container from 'react-bootstrap/Container';
+import React from "react";
+import Button from 'react-bootstrap/Button';
+import '../pages/About.css';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+function Home() {
+  return (
+    <div></div>
+
+  );
+}
+
+export default Home;
