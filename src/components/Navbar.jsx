d5d0902ffe512186674dd3892852a86cb2b109a8
Ian M <ianmillichamp.2@gmail.com>
2023-03-10 13:04:08 +0000

    resolved issue of how to adjust background colo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link, } from "react-router-dom";
+import { Link, } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -12,7 +12,7 @@ import ReactPlayer from 'react-player'
 
 
 
-const Navigation = () =>{
+const Navigation = () => {
   return (
     <Navbar expand="lg" className='navigation'>
       <ReactPlayer url="https://drive.google.com/file/d/1CU5UrGA1ldV69O4nwkPXtCeF7r7W9avE/view?usp=sharing" />
@@ -20,33 +20,33 @@ const Navigation = () =>{
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            </Nav>
-            <Nav >
-            <Link style={{ color: '#6CA6C1',  }} to="/" className="home" activeClassName="selected"><h1>Home</h1></Link>
-            <Link style={{ color: '#6CA6C1' }} to="/About" className="about"><h1>About </h1></Link>
-            <Link style={{ color: '#6CA6C1' }} to="/Projects" className="projects"><h1>Projects</h1></Link>
-            <Link style={{ color: '#6CA6C1' }} to={"https://drive.google.com/file/d/1K5jShP_8ROMvbdFBl_IGcTLy-VllgJRN/view?usp=sharing"}className="resume"><h1>Resume</h1></Link>
-            <NavDropdown  title="Lets Connect !" id="connect-nav-dropdown">
-            <NavDropdown.Item >
-            <Link style={{ color: '#2f3061' }} to="/Contact"><i className="fa fa-address-card"></i> Contact Form</Link>
-              
+          </Nav>
+          <Nav >
+            <Link style={{ color: '#2F3061', }} to="/" className="home" activeClassName="selected"><h1>Home</h1></Link>
+            <Link style={{ color: '#2F3061' }} to="/About" className="about"><h1>About </h1></Link>
+            <Link style={{ color: '#2F3061' }} to="/Projects" className="projects"><h1>Projects</h1></Link>
+            <Link style={{ color: '#2F3061' }} to={"https://drive.google.com/file/d/1K5jShP_8ROMvbdFBl_IGcTLy-VllgJRN/view?usp=sharing"} className="resume"><h1>Resume</h1></Link>
+            <NavDropdown title="Lets Connect !" id="connect-nav-dropdown">
+              <NavDropdown.Item id="drop" >
+                <Link style={{ color: '#2F3061' }} to="/Contact"><h2><i className="fa fa-address-card"></i> Contact Form</h2></Link>
+
               </NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="tel:+4407498174271">
-              <i className="fa fa-phone"></i> Phone
+              <NavDropdown.Item id="drop" href="tel:+4407498174271">
+                <i className="fa fa-phone"></i> Phone
               </NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="mailto:ianmillichamp.2@gmail.com"><i className="fa fa-envelope"></i> Email </NavDropdown.Item>
+              <NavDropdown.Item id="drop" href="mailto:ianmillichamp.2@gmail.com"><i className="fa fa-envelope"></i> Email </NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="https://www.linkedin.com/in/ian-millichamp/">
-              <i className="fa fa-linkedin"></i> LinkedIn
+              <NavDropdown.Item id="drop" href="https://www.linkedin.com/in/ian-millichamp/">
+                <i className="fa fa-linkedin"></i> LinkedIn
               </NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="https://github.com/IanMilli">
-              <i className="fa fa-github"></i> GitHub
+              <NavDropdown.Item id="drop" href="https://github.com/IanMilli">
+                <i className="fa fa-github"></i> GitHub
               </NavDropdown.Item>
             </NavDropdown>
-           
+
           </Nav>
         </Navbar.Collapse>
       </Container>
