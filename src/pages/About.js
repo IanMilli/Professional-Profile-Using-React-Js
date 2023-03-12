f66d51bf3ff964f5709221afd22b55cb0a76e1f3
Ian M <ianmillichamp.2@gmail.com>
2023-03-12 15:59:11 +0000

    was not happy with result so looked at different solution and ran npm install react-scroll-motion

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import React from "react";
+import "animate.css/animate.min.css";
+import { AnimationOnScroll } from 'react-animation-on-scroll';
 import profilePic from '../assets/images/profilepic.jpg';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
@@ -15,7 +17,15 @@ function Home() {
                     <img src={profilePic} alt="Profile Pic" className="profilePic"/>
                     </Col>
                     <Col xs={8}>
-
+                    <ScrollAnimation animateIn='fadeIn'
+  animateOut='fadeOut'>
+  <h1>
+    React Animate On Scroll
+  </h1>
+  <h2>
+    Using:
+  </h2>
+</ScrollAnimation>
                     </Col>
                 </Row>
                
