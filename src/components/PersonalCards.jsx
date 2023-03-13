74bcc0ab6683659229d73968dbf5d651352d3aed
Ian M <ianmillichamp.2@gmail.com>
2023-03-13 21:11:59 +0000

    added planned carousels

--- src/components/PersonalCards.jsx
+++ src/components/PersonalCards.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import cn from "classnames";
 import {Link, } from "react-router-dom";
+import Carousel from 'react-bootstrap/Carousel';
 
-function personalFlipCard({ card }) {
+function PersonalFlipCard({ card }) {
   const [showBack, setShowBack] = useState(false);
 
   function handleClick() {
@@ -43,6 +44,45 @@ function personalFlipCard({ card }) {
           <div className="card-body text-center">
             <h1 className="text-white">{card.title}</h1>
             <p className="card-text fs-1 fw-bold">{card.front}</p>
+            <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=First slide&bg=373940"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Second slide&bg=282c34"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Third slide&bg=20232a"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
           </div>
         </div>
         <div className="card back">
@@ -56,4 +96,4 @@ function personalFlipCard({ card }) {
   );
 }
 
-export default personalFlipCard;
+export default PersonalFlipCard;
--- src/pages/Personal.js
+++ src/pages/Personal.js
@@ -1,6 +1,6 @@
 import React from "react";
-import FlipCard from "../components/ProjectCard.jsx";
-
+import FlipCard from "../components/PersonalCards.jsx";
+import homePic from "../assets/images/homePage.jpg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../components/scss/ProjectCard.scss";
 
@@ -9,7 +9,7 @@ const cards = [
   {
     id: "1",
     variant: "hover",
-    title: "Solo Authored Web Development Projects",
+    title: "Solo Authored Web Skill Development Projects",
     front: "",
     back: "Click the link below to learn more about Web Applications I have Created Myself",
     address: "/Projects/Personal/Solo",
@@ -18,44 +18,46 @@ const cards = [
   {
     id: "2",
     variant: "hover",
-    title: "Professional Web Development Work",
+    title: "Collaborated Web Skill Development Projects",
     front: "",
     back: "Click the link below to learn more Web Applications I have Created In Collaboration With Others",
     address: "/Projects/Personal/Collaboration",
     linkTitle: "Collaborated on Projects"
   },
- 
+
 ];
 
 
 function Personal() {
   return (
     <div style={{
-      backgroundImage: `url("../images/homeBackground.jpg")`,
+      backgroundImage: `url(${homePic})`,
 
       backgroundRepeat: 'no-repeat',
       backgroundSize: 'cover',
       width: 'auto',
-      height: '120vh'
+      height: '140vh'
     }}>
+     
+        <div className="textProjectsHeader">To Review the Web Design Projects I have completed in the development of my technical skills </div>
+        <div className="textProjectsHeader"> select from the following: </div>
 
-      <h3>To Review Web Development Projects I have been involved select from the following</h3>
-      
 
-      <div className="container">
-        <div className="row h-100">
-          <div class="col d-flex flex-column flex-md-row justify-content-around align-items-center">
-            {cards.map((card) => (
-              <FlipCard key={card.id} card={card} />
-            ))}
+        <div className="container">
+          <div className="row h-100">
+            <div class="col d-flex flex-column flex-md-row justify-content-around align-items-center">
+              {cards.map((card) => (
+                <FlipCard key={card.id} card={card} />
+              ))}
+            </div>
           </div>
         </div>
-      </div>
-      </div>)
+     
+    </div>)
 
 }
 
 
 
 
-      export default Personal;
+export default Personal;
